478ec576721f3c87f54b6a7e96cecbcb18424bf2
Shawn Cicoria <github@cicoria.com>
2015-10-14 19:39:00 -0400

    fixup cache

--- app.js
+++ app.js
@@ -8,13 +8,21 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+var options = {
+  etag: false,
+  maxAge: '0d',
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+  }};
+
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), options));
 
 
 // catch 404 and forward to error handler
@@ -35,7 +43,7 @@ if (app.get('env') === 'development') {
       message: err.message,
       error: err
     });
-  });
+  });  
 }
 
 // production error handler
